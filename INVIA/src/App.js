c1a4608d09a8fa009bd3d71d2ba65eb27f60e341
SpectreLion <jose.tin96@hotmail.com>
2018-04-09 13:46:57 -0500

    Tercera Modificacion
    Implemento de librerias , pantallas iniciales e imagenes necesarias

--- INVIA/src/App.js
+++ INVIA/src/App.js
@@ -1,58 +1,35 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
+ *INVIA
+ *@authors: Jose Luis Tinajero,Ivan Ruiz, Miguel Guevara, Pablo Rodriguez, Juan Pablo Vazquez
+ *
  */
 
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+  StackNavigator,
+} from 'react-navigation';
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+import MenuScreen from './screens/MenuScreen';
+import * as firebase from 'firebase';
+// Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyDxLo-_q_b4hwJTnil8TaJNCWneFB8KKp0",
+    authDomain: "invia-5b246.firebaseapp.com",
+    databaseURL: "https://invia-5b246.firebaseio.com",
+    projectId: "invia-5b246",
+    storageBucket: "invia-5b246.appspot.com",
+    messagingSenderId: "499747991252"
+  };
+  firebase.initializeApp(config);
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default App = StackNavigator(
+    {
+      Home: { screen: HomeScreen },
+      Login: { screen: LoginScreen },
+      Menu: { screen: MenuScreen }
+    },
+    {
+      headerMode: 'none'
+    }
+);
---
+++ INVIA/src/screens/HomeScreen.js
@@ -0,0 +1,71 @@
+/*
+**
+ * RemindMe App
+ * home Screen
+ */
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  Image,
+  View,
+  StatusBar
+} from 'react-native';
+import Colors from './../theme/colors';
+
+export default class HomeScreen extends Component<{}> {
+
+  goToTaskScreen(){
+    this.props.navigation.navigate('Login');
+  }
+
+  render() {
+
+    return (
+      <View style={styles.container}>
+        <StatusBar
+          barStyle="light-content"
+       />
+        <Image
+          style={styles.remindMeLogo}
+          source={require('./../images/logo_invia.png')}/>
+        <TouchableHighlight
+          style={styles.homeButton}
+          underlayColor={Colors.touchableHover}
+          onPress={this.goToTaskScreen.bind(this)}>
+          <Text style={styles.homeButtonText}>COMENZAR</Text>
+        </TouchableHighlight>
+        <Image source={require('./../images/avatars.png')} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: Colors.primaryColorDarker,
+  },
+  remindMeLogo:{
+    marginTop: 80
+  },
+  homeButton: {
+      width: 325,
+      height: 55,
+      paddingTop: 15,
+      borderRadius: 27,
+      borderStyle: 'solid',
+      borderWidth: 2,
+      borderColor: '#ffffff'
+  },
+  homeButtonText: {
+    fontFamily: 'Avenir',
+    textAlign: 'center',
+    fontSize: 17,
+    color: 'white'
+  }
+});
