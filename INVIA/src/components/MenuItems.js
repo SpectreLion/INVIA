0ed64d0a35cd78292f046256e9684cf10c183ee4
mike18G <migueguevara3@gmail.com>
2018-04-17 19:43:16 -0500

    Plantilla Pantallas

---
+++ INVIA/src/components/MenuItems.js
@@ -0,0 +1,61 @@
+/**
+ * INVIA App
+ * MenuItems  Component
+ */
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  Image,
+  View,
+  Dimensions
+} from 'react-native';
+import Colors from './../theme/colors';
+import Typografy from './../theme/typografy';
+
+
+const width = Dimensions.get('window').width;
+export default class MenuItems extends Component<{}> {
+
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    const { id,title,image } = this.props;
+    return (
+        <TouchableHighlight
+            underlayColor={Colors.touchableHover}>
+          <View style={styles.container}>
+            <Image
+              style={styles.icon}
+              source={image}/>
+            <Text style={ styles.categoryText }>{title}</Text>
+          </View>
+        </TouchableHighlight>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    width:width/3-15,
+    height:230,
+    marginLeft:5,
+  },
+  icon: {
+    width:100,
+    height:100
+  },
+  categoryText: {
+    ...Typografy.avenirFont,
+    color: Colors.categoriesText,
+    fontSize: 14,
+    textAlign:'center',
+    marginTop:10
+  }
+});
